d40f9372c5ad3897991ac7dc3df8d437b9937b5e
bbdotqchicken1 <digitalmarketing@bbdotqusa.com>
2022-02-24 08:48:18 -0500

    upload files

---
+++ qr-menu/index.php
@@ -0,0 +1,6 @@
+<div style="">
+  <img src="../images/bbq/1.-bbq_fallschurch_dine-in-front.jpg" style="height:auto; width:100%;" />
+</div>
+<div style="">
+  <img src="../images/bbq/2.-bbq_fallschurch_dine-in-back.jpg" style="height:auto; width:100%;" />
+</div>
---
+++ qrcode-menu/index.php
@@ -0,0 +1,3 @@
+<div style="">
+  <img src="../images/bbq/bbq_fallschurch_qr_100620-01.jpg" style="height:auto; width:100%;" />
+</div>
